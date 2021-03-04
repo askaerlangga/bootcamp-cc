c1f3fd2331cf60a2aadea0002062722c28d54f08
askaerlangga <ae.askaerlangga@gmail.com>
2021-03-04 15:48:44 +0700

    Add files

---
+++ aritmatika.php
@@ -0,0 +1,8 @@
+<?php 
+    $nilai1 = 2;
+    $nilai2 = 30;
+    $hasil = $nilai1 + $nilai2;
+    echo "Nilai1 : $nilai1" . PHP_EOL;
+    echo "Nilai2 : $nilai2" . PHP_EOL;
+    echo "Hasil dari $nilai1 + $nilai2 = $hasil" . PHP_EOL;
+ ?>
---
+++ table.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div class="container">
+        <div>
+        <h1 style="margin: 30px 0 20px 0; text-align:center">Daftar Hadir</h1>
+            <table style="text-align: center;" class="table table-hover">
+                <thead style="text-align: center;">
+                    <tr>
+                        <th scope="col">Nomor</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Alamat</th>
+                        <th scope="col">Opsi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Aska</td>
+                        <td>Serang</td>
+                        <td>
+                            <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                            <button type="button" class="btn btn-warning btn-sm">Edit</button>
+                            <button type="button" class="btn btn-primary btn-sm">View</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Joko</td>
+                        <td>Serang</td>
+                        <td>
+                            <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                            <button type="button" class="btn btn-warning btn-sm">Edit</button>
+                            <button type="button" class="btn btn-primary btn-sm">View</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Udin</td>
+                        <td>Serang</td>
+                        <td>
+                            <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                            <button type="button" class="btn btn-warning btn-sm">Edit</button>
+                            <button type="button" class="btn btn-primary btn-sm">View</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
