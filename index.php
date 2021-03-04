38997884895aeef76dc20e9495f1bbbe84a29e84
askaerlangga <ae.askaerlangga@gmail.com>
2021-03-04 23:33:50 +0700

    Update login page

--- index.php
+++ index.php
@@ -1,41 +1,55 @@
 <html>
+
 <head>
-	<title>Membuat Login Multi User Level Dengan PHP dan MySQLi - www.malasngoding.com</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Login</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
 </head>
+
 <body>
- 
-	<h1>Membuat Login Multi User Level Dengan PHP dan MySQLi <br/> www.malasngoding.com</h1>
- 
-	<?php 
-	if(isset($_GET['pesan'])){
-		if($_GET['pesan']=="gagal"){
-			echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
-		}
-	}
-	?>
- 
-	<div class="kotak_login">
-		<p class="tulisan_login">Silahkan login</p>
- 
-		<form action="cek_login.php" method="post">
-			<label>Username</label>
-			<input type="text" name="username" class="form_login" placeholder="Username .." required="required">
- 
-			<label>Password</label>
-			<input type="password" name="password" class="form_login" placeholder="Password .." required="required">
- 
-			<input type="submit" class="tombol_login" value="LOGIN">
- 
-			<br/>
-			<br/>
-			<center>
-				<a class="link" href="https://www.malasngoding.com">kembali</a>
-			</center>
-		</form>
-		
+
+	
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col"></div>
+			<div class="col formlogin">
+			<h1 class="judul">Login</h1>
+			<form action="cek_login.php" method="post">
+				<?php 
+				if(isset($_GET['pesan'])){
+					if($_GET['pesan']=="gagal"){
+						echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>Username dan Password tidak sesuai !<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>";
+					}
+				}
+				?>
+				<div class="mb-3">
+					<label for="exampleInputEmail1" class="form-label">Username</label>
+					<input type="text" name="username" class="form-control" id="exampleInputEmail1" placeholder="Username" required="required">
+				</div>
+				<div class="mb-3">
+					<label for="exampleInputPassword1" class="form-label">Password</label>
+					<input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required="required">
+				</div>
+				
+				<div class="d-grid gap-2">
+				<button type="submit" class="btn buttonlogin">Login</button>
+				</div>
+				
+			</form>
+			</div>
+			<div class="col"></div>
+		</div>
 	</div>
- 
- 
+	
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+	</script>
 </body>
+
 </html>
