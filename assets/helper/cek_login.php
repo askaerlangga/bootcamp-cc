48306ac60c96547382a7180b00e2fc115fb3ed3f
askaerlangga <ae.askaerlangga@gmail.com>
2021-03-05 14:36:05 +0700

    Menghapus level user

--- assets/helper/cek_login.php
+++ assets/helper/cek_login.php
@@ -17,41 +17,9 @@ $cek = mysqli_num_rows($login);
 
 // cek apakah username dan password di temukan pada database
 if($cek > 0){
-
-	$data = mysqli_fetch_assoc($login);
-
-	// cek jika user login sebagai admin
-	if($data['level']=="admin"){
-
-		// buat session login dan username
-		$_SESSION['username'] = $username;
-		$_SESSION['level'] = "admin";
-		// alihkan ke halaman dashboard admin
-		header("location:page/mahasiswa.php");
-
-	// cek jika user login sebagai pegawai
-	}else if($data['level']=="pegawai"){
-		// buat session login dan username
-		$_SESSION['username'] = $username;
-		$_SESSION['level'] = "pegawai";
-		// alihkan ke halaman dashboard pegawai
-		header("location:halaman_pegawai.php");
-
-	// cek jika user login sebagai pengurus
-	}else if($data['level']=="pengurus"){
-		// buat session login dan username
-		$_SESSION['username'] = $username;
-		$_SESSION['level'] = "pengurus";
-		// alihkan ke halaman dashboard pengurus
-		header("location:halaman_pengurus.php");
-
-	}else{
-
-		// alihkan ke halaman login kembali
-		header("location:index.php?pesan=gagal");
-	}	
+	header("location:../../page/mahasiswa.php");
 }else{
-	header("location:index.php?pesan=gagal");
+	header("location:../../index.php?pesan=gagal");
 }
 
 ?>
